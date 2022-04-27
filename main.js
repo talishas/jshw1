b600919ca4870ab621e812b99feed2a211ab8141
talishas <103214424+talishas@users.noreply.github.com>
2022-04-27 23:56:11 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+// console.log(a1) ;
+// console.log(a2) ;
+// console.log(a3) ;
+// console.log(a4) ;
+// console.log(a5) ;
+// console.log(a6) ;
+// console.log(a7) ;
+// console.log(a8) ;
+// console.log(a9) ;
+// console.log(a10) ;
+// console.log(a11) ;
+// console.log(a12) ;
+// console.log(a13) ;
+
+/*let book1 = {
+    title : 'Harry Potter',
+    pages : 356,
+    genre : 'fantasy',
+};
+console.log(book1);*/
+
+/*let book2 = {
+    title : 'Shantaram',
+    pages : '732',
+    genre : 'Advanture',
+    authors : [
+        'Gregory David Roberts',
+        'John Peter Smith'
+    ]
+}
+console.log(book2);*/
+
+/*let firstname = 'Nataliia';
+let middlename = 'Yuriivna';
+let lastname = 'Kondratovych';
+let person = firstname + ' ' + middlename + ' ' + lastname;
+
+console.log(person);*/
+
+/*
+let firstname = prompt('enter your first name');
+let middlename = prompt('enter your middle name');
+let lastname = prompt('enter your last name');
+
+let person = firstname + ' ' + middlename + ' ' + lastname;
+
+console.log(person);
+*/
+
+/*let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);*/
+
+/*let books = [
+    {title:'Shantaram',pages:732,genre:'novel',authors:['Gregory Davids','John Peter Smith']},
+    {title:'Harry Potter',pages:365,genre:'fantasy',authors:['Rowling','Joan K']},
+    {title:'Bukvarik',pages:10,genre:'history',authors:['Olena','Volosevych']},
+];
+
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);*/
+
+
+
